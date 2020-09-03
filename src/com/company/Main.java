a00cbcd89996a74f9bd00831bf7f5c06b29c201c
stansho <sitanshu.nandan@gmail.com>
2020-09-03 17:34:19 +0530

    Initial commit - this is where we all start.

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Sitanshu");
+        System.out.println("Himanshu");
+        System.out.println("Chitranshu");
+        System.out.println("Shantanu");
+        System.out.println("Koryaaaa");
+    }
+}
